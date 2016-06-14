01fe589db92a52da165dc84a44c7812a48fd0419
Kamil Wójcik <van_kam@wp.pl>
2016-06-14 14:31:21 +0200

    Initial Commit

---
+++ Przypominajka/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Przypominajka
+//
+//  Created by Kamil Wójcik on 14.06.2016.
+//  Copyright © 2016 Kamil Wójcik. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
